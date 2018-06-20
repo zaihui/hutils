16a40006c10a4cc69c5b6be80075041555f68159
suziyue <suziyue@kezaihui.com>
2018-06-20 22:59:07 +0800

    feat: add empty package as 0.0.1

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='hutils',
+    version='0.0.1',
+    description='a charming python web util-library',
+    long_description='',
+    author='ZaiHui Dev',
+    author_email='llk@kezaihui.com',
+    url='https://github.com/zaihui/hutils/',
+    license='MIT License',
+    packages=['hutils'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
