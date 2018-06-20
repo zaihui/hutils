1d50ed10a64c95c16dd14e49b94e9b21f759fcb3
suziyue <suziyue@kezaihui.com>
2018-06-21 00:23:16 +0800

    feat: add flake8 and pytest

---
+++ tests/test_import.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import unittest
+
+
+class ImportTests(unittest.TestCase):
+    def test_import(self):
+        import hutils
+        self.assertTrue(hutils)
