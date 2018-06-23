01dbbed5a0c894ede3e79d5c0d3f26f92c2551ee
Lirian Su <liriansu@gmail.com>
2018-06-23 16:12:59 +0800

    Bump version: 0.0.2 → 0.0.3

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -4,7 +4,7 @@ from .data_types import bytes_to_str, get_data, merge_dicts, quantize
 from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import get_uid
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = [
     'EmptyContextManager',
