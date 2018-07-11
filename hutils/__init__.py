bd0ce5e1157d28aeb2ce8127ee01dba2874edecd
Lirian Su <liriansu@gmail.com>
2018-07-11 15:28:56 +0800

    feat: add format_json to api

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .classes import EmptyContextManager, TupleEnum
-from .data_types import bytes_to_str, get_data, merge_dicts, quantize
+from .data_types import bytes_to_str, format_json, get_data, merge_dicts, quantize
 from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import datetime_combine, get_uid
 from .validators import is_uuid
@@ -11,6 +11,7 @@ __all__ = [
     'EmptyContextManager',
     'TupleEnum',
     'bytes_to_str',
+    'format_json',
     'get_data',
     'merge_dicts',
     'quantize',
