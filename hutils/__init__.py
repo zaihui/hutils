7e6d040d1d89a9cc49610b00ff181072550cbed2
Lirian Su <liriansu@gmail.com>
2018-07-17 21:44:33 +0800

    feat: add is_int

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -2,8 +2,8 @@
 from .classes import EmptyContextManager, TupleEnum
 from .data_types import bytes_to_str, format_json, get_data, merge_dicts, quantize
 from .schemas import get_offset_and_limit, get_start_and_end_time
-from .shortcuts import datetime_combine, get_uid
-from .validators import is_uuid
+from .shortcuts import datetime_combine, get_uid, list_get
+from .validators import is_int, is_uuid
 
 __version__ = '0.0.5'
 
@@ -19,5 +19,7 @@ __all__ = [
     'get_start_and_end_time',
     'datetime_combine',
     'get_uid',
+    'list_get',
+    'is_int',
     'is_uuid',
 ]
--- hutils/validators.py
+++ hutils/validators.py
@@ -15,3 +15,16 @@ def is_uuid(string):
         return bool(string and uuid.UUID(string).hex == string)
     except ValueError:
         return False
+
+
+def is_int(string):
+    """ 检查字符串是不是合法的 int. validate if string is a valid int.
+    Examples:
+        >>> if is_int('wrong string'): ...
+
+    Returns: bool
+    """
+    try:
+        return bool(int(string) or True)
+    except ValueError:
+        return False
