d4b9a5f1baebd036a3a16ff385450620ad8593d9
Lirian Su <liriansu@gmail.com>
2018-07-25 19:12:20 +0800

    Bump version: 0.0.6 → 0.0.7

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -5,7 +5,7 @@ from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import datetime_combine, get_uid, list_get
 from .validators import is_int, is_uuid
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 __all__ = [
     'EmptyContextManager',
