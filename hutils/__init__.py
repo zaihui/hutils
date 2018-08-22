5dd9add42717ced815c17b34d10bb8d9d0a57daf
Lirian Su <liriansu@gmail.com>
2018-08-22 16:22:27 +0800

    feat: add is_chinese_phone

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -3,7 +3,7 @@ from .classes import EmptyContextManager, TupleEnum
 from .data_types import bytes_to_str, format_json, get_data, merge_dicts, normalize, quantize
 from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import datetime_combine, get_uid, list_get
-from .validators import is_int, is_uuid
+from .validators import is_chinese_phone, is_int, is_uuid
 
 __version__ = '0.0.7'
 
@@ -21,6 +21,7 @@ __all__ = [
     'datetime_combine',
     'get_uid',
     'list_get',
+    'is_chinese_phone',
     'is_int',
     'is_uuid',
 ]
--- hutils/validators.py
+++ hutils/validators.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 # this module provides various validators
+import re
 import uuid
 
 
 def is_uuid(string):
     """ 检查字符串是不是合法的 UUID。validate if string is a valid uuid.
+
     Examples:
         >>> if is_uuid('wrong string'): ...
 
-    Returns: bool
+    :rtype: bool
     """
     try:
         return bool(string and uuid.UUID(string).hex == string)
@@ -19,13 +21,28 @@ def is_uuid(string):
 
 def is_int(string):
     """ 检查字符串是不是合法的 int. validate if string is a valid int.
+
     Examples:
         >>> if is_int('wrong string'): ...
 
-    Returns: bool
+    :rtype: bool
     """
     try:
         int(string)
         return True
     except ValueError:
         return False
+
+
+CHINESE_PHONE_REGEX = re.compile(r'^1[3-9][0-9]{9}$')
+
+
+def is_chinese_phone(string):
+    """ 检查字符串是不是合法的大陆手机号。validate if string is a valid chinese mainland phone number.
+
+    Examples
+        >>> if is_chinese_phone('12345678910'): ...
+
+    :rtype: bool
+    """
+    return CHINESE_PHONE_REGEX.match(string)
