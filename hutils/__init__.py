052344f44c55bd6f01b7d675934c216d051c3971
Lirian Su <liriansu@gmail.com>
2018-08-28 21:42:14 +0800

    Bump version: 0.1.2 → 0.1.3

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -5,7 +5,7 @@ from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import datetime_combine, get_uid, list_first, list_get, mock_lambda
 from .validators import is_chinese_phone, is_int, is_uuid
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = [
     'EmptyContextManager',
