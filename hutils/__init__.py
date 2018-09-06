8bc4ebb81c518f8cca666d2ba2e6ea0b6ecdc810
Lirian Su <liriansu@gmail.com>
2018-09-06 16:38:13 +0800

    Bump version: 0.1.4 → 0.1.5

--- hutils/__init__.py
+++ hutils/__init__.py
@@ -6,7 +6,7 @@ from .schemas import get_offset_and_limit, get_start_and_end_time
 from .shortcuts import datetime_combine, get_uid, list_first, list_get, mock_lambda
 from .validators import is_chinese_phone, is_int, is_uuid
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __all__ = [
     'django',
