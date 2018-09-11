e75071358b514832c01cb841792162858642e06b
Lirian Su <liriansu@gmail.com>
2018-09-11 21:28:04 +0800

    feat: add delta_days for datetime_combine

--- hutils/shortcuts.py
+++ hutils/shortcuts.py
@@ -4,17 +4,26 @@
 from __future__ import absolute_import, unicode_literals
 
 import datetime
-from typing import Iterable, Tuple, Type
+from typing import Iterable, Optional, Tuple, Type
 
 
 def datetime_combine(
-        start_date: datetime.date, end_date: datetime.date = None) -> Tuple[datetime.datetime, datetime.datetime]:
+        start_date: datetime.date, end_date: datetime.date = None,
+        delta_days: Optional[int] = None) -> Tuple[datetime.datetime, datetime.datetime]:
     """ 获取一段日期的起止时间。get start/end datetime from date.
 
     Examples::
 
         start, end = datetime_combine(datetime.date.today())
+        yesterday, today = datetime_combine(datetime.date.today(), delta_days=-1)
+        today, a_week_after = datetime_combine(datetime.date.today(), delta_days=7)
     """
+    if end_date and delta_days is not None:
+        raise ValueError('Can not specify end_date and delta_days at the same time')
+    if delta_days is not None:
+        start_date, end_date = start_date, start_date + datetime.timedelta(days=delta_days)
+        if delta_days < 0:
+            start_date, end_date = end_date, start_date
     end_date = end_date or start_date
     return (
         datetime.datetime.combine(start_date, datetime.time.min),
