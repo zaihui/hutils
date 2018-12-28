871f3c630c3bda03f9c2f522f4819c83ce332a12
Lirian Su <liriansu@gmail.com>
2018-12-28 12:17:05 +0800

    feat: quote default

--- hutils/django/migrations.py
+++ hutils/django/migrations.py
@@ -44,7 +44,8 @@ class AlterDefault(Operation):
         if to_field.default != NOT_PROVIDED:
             table_name = schema_editor.quote_name(meta.db_table)
             column = schema_editor.quote_name(to_field.column)
-            schema_editor.execute(f'ALTER TABLE {table_name} ALTER COLUMN {column} SET DEFAULT {to_field.default}')
+            default = schema_editor.quote_name(to_field.default)
+            schema_editor.execute(f'ALTER TABLE {table_name} ALTER COLUMN {column} SET DEFAULT {default}')
 
     def database_backwards(self, app_label, schema_editor: BaseDatabaseSchemaEditor, from_state, to_state):
         self.database_forwards(app_label, schema_editor, from_state, to_state)
